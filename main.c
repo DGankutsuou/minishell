f8274822f3296b2676511c22523eb07c05dd977e
DGankutsuou <DGankutsuou@github.com>
2025-03-06 17:19:04 +0000

    blessed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: blessed <blessed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 13:36:15 by blessed           #+#    #+#             */
-/*   Updated: 2025/03/06 16:46:10 by blessed          ###   ########.fr       */
+/*   Updated: 2025/03/06 17:18:35 by blessed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,20 @@ char	*read_line()
 
 int	main(int argc, char **argv)
 {
-	while (1)
+	// 1: get line
+	// 2: parce
+	// 3: execute
+	// 4: loop
+	// compile with -lreadline
+	char	*line;
+
+	line = read_line();
+	while (line)
 	{
-		char	*line;
+		printf ("%s\n", line);
 
-		// 1: get line
+
 		line = read_line(); // todo
-		//printf ("%s\n", line);
-		// 2: parce
-
-		// 3: execute
 	}
 	return (0);
 }
